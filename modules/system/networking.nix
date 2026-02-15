d0a5e4e180b33e7a7da0cc77a8fdd6cd8e6f09ef
Amir Abdullaev <me@sqxt.dev>
2026-02-15 21:57:55 +0300

    fix

--- modules/system/networking.nix
+++ modules/system/networking.nix
@@ -1,11 +1,11 @@
 { ... }:
 
 {
+  networking.nftables.enable = true;
   networking.networkmanager.enable = true;
 
   networking.firewall = {
     enable = true;
-    allowPing = true;
     allowedTCPPorts = [ ];
     allowedUDPPorts = [ ];
   };
