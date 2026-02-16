c3159350b51a0525ae9b6d2f4c11f3c7f654d537
Amir Abdullaev <me@sqxt.dev>
2026-02-16 21:03:22 +0300

    add vim keybindings

--- modules/home/nixvim.nix
+++ modules/home/nixvim.nix
@@ -43,6 +43,51 @@
           silent = true;
         };
       }
+      {
+        mode = "n";
+        key = "<leader>ff";
+        action = "<cmd>Telescope find_files<CR>";
+        options = {
+          desc = "Find files";
+          silent = true;
+        };
+      }
+      {
+        mode = "n";
+        key = "<leader>fg";
+        action = "<cmd>Telescope live_grep<CR>";
+        options = {
+          desc = "Grep files";
+          silent = true;
+        };
+      }
+      {
+        mode = "n";
+        key = "<leader>fb";
+        action = "<cmd>Telescope buffers<CR>";
+        options = {
+          desc = "Find buffers";
+          silent = true;
+        };
+      }
+      {
+        mode = "n";
+        key = "<leader>fh";
+        action = "<cmd>Telescope help_tags<CR>";
+        options = {
+          desc = "Find help";
+          silent = true;
+        };
+      }
+      {
+        mode = "n";
+        key = "<leader>fr";
+        action = "<cmd>Telescope oldfiles<CR>";
+        options = {
+          desc = "Recent files";
+          silent = true;
+        };
+      }
     ];
 
     colorschemes.catppuccin = {
