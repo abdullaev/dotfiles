ef8d5df4afe49c4618431d0647abf761b34ab1c9
Amir Abdullaev <me@sqxt.dev>
2026-02-21 18:18:15 +0300

    fix

--- modules/home/core/programs/neovim/default.nix
+++ modules/home/core/programs/neovim/default.nix
@@ -32,10 +32,18 @@
 
           lazy.enable = true;
           autocomplete.blink-cmp.enable = true;
-          binds.whichKey.enable = true;
+          binds.whichKey = {
+            enable = true;
+            setupOpts = {
+              preset = "helix";
+            };
+          };
 
-          visuals.tiny-devicons-auto-colors.enable = true;
-          statusline.lualine.enable = true;
+          statusline.lualine = {
+            enable = true;
+            activeSection = {
+            };
+          };
 
           utility.direnv.enable = true;
           utility.nix-develop.enable = true;
@@ -50,11 +58,91 @@
                 replace_netrw = true;
                 trash = true;
               };
-              picker.enabled = true;
+              picker = {
+                enabled = true;
+
+                icons = {
+                  files = {
+                    dir_open = " ";
+                  };
+                };
+
+                previewers = {
+                  diff = {
+                    style = "syntax";
+                  };
+                };
+              };
+              notifier = {
+                enabled = true;
+              };
+              dashboard = {
+                preset = {
+                  keys = [
+                    {
+                      icon = " ";
+                      key = "f";
+                      desc = "Find File";
+                      action = ":lua Snacks.dashboard.pick('files')";
+                    }
+                    {
+                      icon = " ";
+                      key = "n";
+                      desc = "New File";
+                      action = ":ene | startinsert";
+                    }
+                    {
+                      icon = " ";
+                      key = "g";
+                      desc = "Find Text";
+                      action = ":lua Snacks.dashboard.pick('live_grep')";
+                    }
+                    {
+                      icon = " ";
+                      key = "r";
+                      desc = "Recent Files";
+                      action = ":lua Snacks.dashboard.pick('oldfiles')";
+                    }
+                    {
+                      icon = " ";
+                      key = "q";
+                      desc = "Quit";
+                      action = ":qa";
+                    }
+                  ];
+                };
+                sections = [
+                  {
+                    section = "header";
+                  }
+                  {
+                    section = "keys";
+                    padding = 1;
+                  }
+                  {
+                    icon = " ";
+                    title = "Recent Files";
+                    section = "recent_files";
+                    indent = 1;
+                    padding = 1;
+                  }
+                  {
+                    icon = " ";
+                    title = "Projects";
+                    section = "projects";
+                    indent = 1;
+                    padding = 1;
+                  }
+                ];
+              };
             };
           };
 
-          ui.fastaction.enable = true;
+          mini = {
+            basics = {
+              enable = true;
+            };
+          };
 
           lsp = {
             enable = true;
@@ -95,6 +183,17 @@
           };
 
           debugger.nvim-dap.enable = true;
+
+          ui.noice = {
+            enable = true;
+            setupOpts = {
+              messages = {
+                view_search = "mini";
+              };
+            };
+          };
+
+          visuals.tiny-devicons-auto-colors.enable = true;
 
           luaConfigPost = ''
             vim.fn.sign_define("DapBreakpoint", { text = "", texthl = "DiagnosticError" })
@@ -139,12 +238,6 @@
               Snacks.picker.git_log_file()
             end, { desc = "Git commits (file)" })
           '';
-
-          mini = {
-            basics.enable = true;
-            icons.enable = true;
-            notify.enable = true;
-          };
 
           git = {
             enable = true;
