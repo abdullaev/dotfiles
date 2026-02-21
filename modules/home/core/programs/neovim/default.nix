aa12b4351a56f8582c4dc6662e3fe28efb178626
Amir Abdullaev <me@sqxt.dev>
2026-02-21 19:16:06 +0300

    fix

--- modules/home/core/programs/neovim/default.nix
+++ modules/home/core/programs/neovim/default.nix
@@ -42,6 +42,117 @@
           statusline.lualine = {
             enable = true;
             activeSection = {
+              a = [
+                ''
+                  {
+                    "mode",
+                    icons_enabled = true,
+                    fmt = function(str) return str:sub(1,1) end,
+                    separator = { left = "", right = "" },
+                  }
+                ''
+              ];
+              b = [
+                ''
+                  {
+                    "filename",
+                    path = 1,
+                    symbols = { modified = '', readonly = '' },
+                    separator = { left = "", right = "" },
+                  }
+                ''
+              ];
+              c = [
+                ''
+                  {
+                    "diff",
+                    colored = true,
+                    symbols = { added = '+ ', modified = '~ ', removed = '- ' },
+                    separator = { left = "", right = "" },
+                  }
+                ''
+              ];
+              x = [
+                ''
+                  {
+                    function()
+                      local buf_ft = vim.bo.filetype
+                      local excluded_buf_ft = { toggleterm = true, NvimTree = true, ["neo-tree"] = true, TelescopePrompt = true }
+
+                      if excluded_buf_ft[buf_ft] then
+                        return ""
+                      end
+
+                      local bufnr = vim.api.nvim_get_current_buf()
+                      local clients = vim.lsp.get_clients({ bufnr = bufnr })
+
+                      if vim.tbl_isempty(clients) then
+                        return "No Active LSP"
+                      end
+
+                      local active_clients = {}
+                      for _, client in ipairs(clients) do
+                        table.insert(active_clients, client.name)
+                      end
+
+                      return table.concat(active_clients, ", ")
+                    end,
+                    icon = '',
+                    separator = { left = "", right = "" },
+                  }
+                ''
+                ''
+                  {
+                    "diagnostics",
+                    sources = { 'nvim_lsp', 'nvim_diagnostic', 'nvim_diagnostic', 'vim_lsp', 'coc' },
+                    symbols = { error = '󰅙 ', warn = ' ', info = ' ', hint = '󰌵 ' },
+                    colored = true,
+                    update_in_insert = false,
+                    always_visible = false,
+                    diagnostics_color = {
+                      color_error = { fg = 'red' },
+                      color_warn = { fg = 'yellow' },
+                      color_info = { fg = 'cyan' },
+                    },
+                    separator = { left = "", right = "" },
+                  }
+                ''
+              ];
+              y = [
+                ''
+                  {
+                    "branch",
+                    icon = '',
+                    separator = { left = "", right = "" },
+                  }
+                ''
+              ];
+              z = [
+                ''
+                  {
+                    "progress",
+                    separator = { left = "", right = "" }
+                  }
+                ''
+                ''
+                  { 
+                    "location",
+                    separator = { left = "", right = "" }
+                  }
+                ''
+                ''
+                  {
+                    "fileformat",
+                    color = { fg = 'black' },
+                    symbols = {
+                      unix = '',
+                      dos = '',
+                      mac = '',
+                    },
+                    separator = { left = "", right = "" }
+                  }
+                ''
+              ];
             };
           };
 
