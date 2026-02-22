a3f284be6a014f8db6ea17e34a13f2ee43940cb8
Amir Abdullaev <me@sqxt.dev>
2026-02-22 18:44:44 +0300

    fix

--- modules/home/core/programs/neovim/_config/keymaps.nix
+++ modules/home/core/programs/neovim/_config/keymaps.nix
@@ -91,5 +91,17 @@
       lua = true;
       desc = "Git hunk overlay";
     }
+    {
+      mode = "n";
+      key = "<Space>q";
+      action = "<cmd>qa<CR>";
+      desc = "Quit all";
+    }
+    {
+      mode = "n";
+      key = "<Esc>";
+      action = "<cmd>nohlsearch<CR>";
+      desc = "Clear search highlight";
+    }
   ];
 }
