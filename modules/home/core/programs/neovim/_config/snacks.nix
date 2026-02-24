823c203d5fb2ec7621f12b1d0579680baf9f12c1
Amir Abdullaev <me@sqxt.dev>
2026-02-24 19:17:51 +0300

    fix

--- modules/home/core/programs/neovim/_config/snacks.nix
+++ modules/home/core/programs/neovim/_config/snacks.nix
@@ -40,31 +40,26 @@
         preset = {
           keys = [
             {
-              icon = "";
               key = "f";
               desc = "Find File";
               action = ":lua Snacks.dashboard.pick('files')";
             }
             {
-              icon = "";
               key = "n";
               desc = "New File";
               action = ":ene | startinsert";
             }
             {
-              icon = "";
               key = "g";
               desc = "Find Text";
               action = ":lua Snacks.dashboard.pick('live_grep')";
             }
             {
-              icon = "";
               key = "r";
               desc = "Recent Files";
               action = ":lua Snacks.dashboard.pick('oldfiles')";
             }
             {
-              icon = "";
               key = "q";
               desc = "Quit";
               action = ":qa";
@@ -80,14 +75,12 @@
             padding = 1;
           }
           {
-            icon = "";
             title = "Recent Files";
             section = "recent_files";
             indent = 1;
             padding = 1;
           }
           {
-            icon = "";
             title = "Projects";
             section = "projects";
             indent = 1;
