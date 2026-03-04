06b6c1d03fbfd9e1f52b952bfdaced770c5a011c
Amir Abdullaev <me@sqxt.dev>
2026-03-04 12:44:25 +0300

    fix

--- modules/home/core/programs/btop.nix
+++ modules/home/core/programs/btop.nix
@@ -2,6 +2,11 @@
   flake.modules.homeManager.btop = {
     programs.btop = {
       enable = true;
+      settings = {
+        theme_background = false;
+        vim_keys = true;
+        save_config_on_exit = false;
+      };
     };
 
     catppuccin.btop.enable = true;
