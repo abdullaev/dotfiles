bd757f4c29db86d8ff1db49e95f9e5232e963cd1
Amir Abdullaev <me@sqxt.dev>
2026-03-07 19:15:51 +0300

    fix

--- secrets/default.nix
+++ secrets/default.nix
@@ -1,20 +1,21 @@
 {
   age.secrets = {
+    # misc
     access-tokens = {
-      file = ./access-tokens.age;
+      file = ./misc/access-tokens.age;
       group = "wheel";
       mode = "0440";
     };
 
     # vpn
-    awg-estonia.file = ./awg-estonia.conf.age;
-    awg-poland.file = ./awg-poland.conf.age;
-    awg-switzerland.file = ./awg-switzerland.conf.age;
-    rs-awg2-latvia.file = ./rs-awg2-latvia.conf.age;
-    rs-awg2-germany.file = ./rs-awg2-germany.conf.age;
-    rs-awg2-poland.file = ./rs-awg2-poland.conf.age;
-    rs-awg2-kazakhstan.file = ./rs-awg2-kazakhstan.conf.age;
-    rs-awg2-lithuania.file = ./rs-awg2-lithuania.conf.age;
-    rs-awg2-netherlands.file = ./rs-awg2-netherlands.conf.age;
+    awg-estonia.file = ./vpn/awg-estonia.conf.age;
+    awg-poland.file = ./vpn/awg-poland.conf.age;
+    awg-switzerland.file = ./vpn/awg-switzerland.conf.age;
+    rs-awg2-latvia.file = ./vpn/rs-awg2-latvia.conf.age;
+    rs-awg2-germany.file = ./vpn/rs-awg2-germany.conf.age;
+    rs-awg2-poland.file = ./vpn/rs-awg2-poland.conf.age;
+    rs-awg2-kazakhstan.file = ./vpn/rs-awg2-kazakhstan.conf.age;
+    rs-awg2-lithuania.file = ./vpn/rs-awg2-lithuania.conf.age;
+    rs-awg2-netherlands.file = ./vpn/rs-awg2-netherlands.conf.age;
   };
 }
--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -12,15 +12,15 @@ let
   allKeys = userKeys ++ systemKeys;
 in
 {
-  "access-tokens.age".publicKeys = allKeys;
+  "misc/access-tokens.age".publicKeys = allKeys;
 
-  "rs-awg2-latvia.conf.age".publicKeys = allKeys;
-  "rs-awg2-germany.conf.age".publicKeys = allKeys;
-  "rs-awg2-poland.conf.age".publicKeys = allKeys;
-  "rs-awg2-lithuania.conf.age".publicKeys = allKeys;
-  "rs-awg2-netherlands.conf.age".publicKeys = allKeys;
-  "rs-awg2-kazakhstan.conf.age".publicKeys = allKeys;
-  "awg-estonia.conf.age".publicKeys = allKeys;
-  "awg-poland.conf.age".publicKeys = allKeys;
-  "awg-switzerland.conf.age".publicKeys = allKeys;
+  "vpn/rs-awg2-latvia.conf.age".publicKeys = allKeys;
+  "vpn/rs-awg2-germany.conf.age".publicKeys = allKeys;
+  "vpn/rs-awg2-poland.conf.age".publicKeys = allKeys;
+  "vpn/rs-awg2-lithuania.conf.age".publicKeys = allKeys;
+  "vpn/rs-awg2-netherlands.conf.age".publicKeys = allKeys;
+  "vpn/rs-awg2-kazakhstan.conf.age".publicKeys = allKeys;
+  "vpn/awg-estonia.conf.age".publicKeys = allKeys;
+  "vpn/awg-poland.conf.age".publicKeys = allKeys;
+  "vpn/awg-switzerland.conf.age".publicKeys = allKeys;
 }
