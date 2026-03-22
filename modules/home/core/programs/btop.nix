ee9b0fee9206184ac80907fe45bb197de433678a
Amir Abdullaev <me@sqxt.dev>
2026-03-22 21:20:57 +0300

    fix

--- modules/home/core/programs/btop.nix
+++ modules/home/core/programs/btop.nix
@@ -1,14 +1,17 @@
 {
-  flake.modules.homeManager.btop = {
-    programs.btop = {
-      enable = true;
-      settings = {
-        theme_background = false;
-        vim_keys = true;
-        save_config_on_exit = false;
+  flake.modules.homeManager.btop =
+    { pkgs, ... }:
+    {
+      programs.btop = {
+        enable = true;
+        package = pkgs.btop-cuda;
+        settings = {
+          theme_background = false;
+          vim_keys = true;
+          save_config_on_exit = false;
+        };
       };
-    };
 
-    catppuccin.btop.enable = true;
-  };
+      catppuccin.btop.enable = true;
+    };
 }
