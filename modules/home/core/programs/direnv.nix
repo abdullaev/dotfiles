6fac6051de2d99e5aff29b704fcd094d5a9f6914
Amir Abdullaev <me@sqxt.dev>
2026-03-22 19:28:38 +0300

    fix

--- modules/home/core/programs/direnv.nix
+++ modules/home/core/programs/direnv.nix
@@ -5,6 +5,10 @@
       enableZshIntegration = true;
       enableFishIntegration = true;
       nix-direnv.enable = true;
+
+      config = {
+        global.warn_timeout = 0;
+      };
     };
   };
 }
