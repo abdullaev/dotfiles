ff9b4e1cb516f5538b1edfd26460185db46da292
Amir Abdullaev <me@sqxt.dev>
2026-05-02 23:13:00 +0300

    fix

--- modules/home/desktop/firefox.nix
+++ modules/home/desktop/firefox.nix
@@ -1,9 +1,10 @@
 {
   flake.modules.homeManager.firefox =
-    { pkgs, ... }:
+    { pkgs, config, ... }:
     {
       programs.firefox = {
         enable = true;
+        configPath = "${config.xdg.configHome}/mozilla/firefox";
         profiles.default = {
           settings = {
             "widget.use-xdg-desktop-portal.file-picker" = 1;
