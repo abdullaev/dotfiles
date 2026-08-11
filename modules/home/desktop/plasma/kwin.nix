6d0af4a7af9d778082f7736ee6b28cb8be217856
Amir Abdullaev <me@sqxt.dev>
2026-08-11 23:50:21 +0300

    fix

--- modules/home/desktop/plasma/kwin.nix
+++ modules/home/desktop/plasma/kwin.nix
@@ -2,19 +2,29 @@
   flake.modules.homeManager.plasma =
     { osConfig, ... }:
     {
-      programs.plasma.kwin = {
-        virtualDesktops = {
-          number = 4;
-          rows = 1;
-        };
-        nightLight = {
-          enable = true;
-          mode = "location";
-          location = {
-            latitude = toString osConfig.location.latitude;
-            longitude = toString osConfig.location.longitude;
+      programs.plasma = {
+        kwin = {
+          virtualDesktops = {
+            number = 4;
+            rows = 1;
           };
-          temperature.night = 4000;
+          nightLight = {
+            enable = true;
+            mode = "location";
+            location = {
+              latitude = toString osConfig.location.latitude;
+              longitude = toString osConfig.location.longitude;
+            };
+            temperature.night = 4000;
+          };
+        };
+
+        # The night-time daemon computes sunrise/sunset for night light;
+        # Automatic=false pins it to the declared coordinates instead of GeoIP.
+        configFile.knighttimerc.Location = {
+          Automatic = false;
+          Latitude = toString osConfig.location.latitude;
+          Longitude = toString osConfig.location.longitude;
         };
       };
     };
